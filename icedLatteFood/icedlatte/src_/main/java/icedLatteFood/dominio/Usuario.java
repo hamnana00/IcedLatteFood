5f4cc16f9fbf27372120a9f050534c4f2d885b65
dacil <dacil@192.168.1.144>
2024-10-23 00:01:49 +0200

    entidades usuario y cliente

--- icedLatteFood/icedlatte/src_/main/java/icedLatteFood/dominio/Usuario.java
+++ icedLatteFood/icedlatte/src_/main/java/icedLatteFood/dominio/Usuario.java
@@ -1,3 +1,17 @@
 public class Usuario {
-
+	private String idUsuario;
+	private String pass;
+	
+	public String getIdUsuario() {
+		 return idUsuario;
+	}
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
 }
