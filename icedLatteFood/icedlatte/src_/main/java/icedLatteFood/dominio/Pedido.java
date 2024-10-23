ec12f6c93e359e10ea728d6ce75f43e823af22a9
sanafancy <xutingting197@gmail.com>
2024-10-23 18:09:00 +0200

    He completado la clase Pedido

--- icedLatteFood/icedlatte/src_/main/java/icedLatteFood/dominio/Pedido.java
+++ icedLatteFood/icedlatte/src_/main/java/icedLatteFood/dominio/Pedido.java
@@ -1,3 +1,49 @@
 public class Pedido {
-    private
+    private Date fecha;
+    private List<ItemMenu> items;
+    private EstadoPedido estado;
+    private Pago pago;
+
+    public Pedido(Date fecha){
+        this.fecha = fecha;
+        this.items = new ArrayList<>();
+        this.estado = EstadoPedido.PEDIDO;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public List<ItemMenu> getItems() {
+        return items;
+    }
+    public void addItem(ItemMenu item){
+        this.items.add(item);
+    }
+    public void deleteItem(ItemMenu item){
+        this.items.remove(item);
+    }
+
+    public EstadoPedido getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoPedido estado) {
+        this.estado = estado;
+    }
+
+    public Pago getPago() {
+        return pago;
+    }
+
+    public void setPago(Pago pago) {
+        this.pago = pago;
+    }
+    public String toString(){
+        return "Pedido [fecha=" + fecha + ", items=" + items + ", estado=" + estado + ", pago=" + pago + "]";
+    }
 }
