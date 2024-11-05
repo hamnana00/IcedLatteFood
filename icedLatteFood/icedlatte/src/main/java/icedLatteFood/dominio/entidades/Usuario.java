7bf4b5a9239784bf264876717abacb536dd59242
Dacil Jara <146329911+hamnana00@users.noreply.github.com>
2024-11-05 20:19:10 +0100

    Update Usuario.java

--- icedLatteFood/icedlatte/src/main/java/icedLatteFood/dominio/entidades/Usuario.java
+++ icedLatteFood/icedlatte/src/main/java/icedLatteFood/dominio/entidades/Usuario.java
@@ -1,8 +1,21 @@
 package icedLatteFood.dominio.entidades;
-//
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String idUsuario;
+	@Column
 	private String pass;
+
+	public Usuario(String pass) {
+		this.pass=pass;
+	}
 	
 	public String getIdUsuario() {
 		 return idUsuario;
@@ -21,4 +34,9 @@ public class Usuario {
 	public boolean autenticar(String password) {
         return this.pass.equals(password);
     }
+
+	@Override
+	public String toString() {
+		return String.format("Greeting [idUsuario=%s, password=%s]", idUsuario, pass);
+	}
 }
