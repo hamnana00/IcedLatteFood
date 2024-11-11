a54b48432f50949dc109862c7dc2a488eeb5b918
hamnana00 <chaesgf00@gmail.com>
2024-11-11 08:46:21 +0100

    posibles cambios

--- icedLatteFood/icedlatte/src/main/java/icedLatteFood/ServletInitializer.java
+++ icedLatteFood/icedlatte/src/main/java/icedLatteFood/ServletInitializer.java
@@ -2,12 +2,12 @@ package icedLatteFood.icedlatte;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import icedLatteFood.DeliveryApplication;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(IcedlatteApplication.class);
-	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DeliveryApplication.class);
+    }
 }
